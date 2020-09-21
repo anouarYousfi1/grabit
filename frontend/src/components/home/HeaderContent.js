a5e1bea8dd0e859cf3df9165ed5f6a762bd45831
yasTheDreamer <yassine19265@gmail.com>
2020-09-21 10:12:43 +0100

    making the website responsive

--- frontend/src/components/home/HeaderContent.js
+++ frontend/src/components/home/HeaderContent.js
@@ -8,8 +8,33 @@ import { Link } from "react-router-dom";
 const HeaderContent = () => {
   const [Signups, setSignups] = useContext(SignupContext);
 
+  const toggle = () => {
+    const spanMenu = document.querySelector("#menu");
+    const headline = document.querySelector(".HeaderContent__headline");
+    const signup = document.querySelector(".HeaderContent__signup");
+
+    if (spanMenu.style.display === "none") {
+      spanMenu.style.display = "flex";
+      headline.style.display = "none";
+      signup.style.display = "none";
+    } else {
+      spanMenu.style.display = "none";
+      headline.style.display = "flex";
+      signup.style.display = "flex";
+    }
+  };
+
   return (
     <div className="HeaderContent">
+      <span class="menu" id="menu">
+        <Link id="login__menu">Login</Link>
+        <Link to="/signup/driver">Signp as a driver</Link>
+        <Link to="/signup/customer">Signup as a customer</Link>
+      </span>
+      <Link onClick={toggle} class="menuButton">
+        <i class="fa fa-bars fa-lg" aria-hidden="true"></i>
+      </Link>
+
       <Header />
       <div className="HeaderContent__headline">
         <h1>
--- frontend/src/components/profile/Profile.js
+++ frontend/src/components/profile/Profile.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useContext } from "react";
-import { useHistory, Redirect } from "react-router-dom";
+import { useHistory, Redirect, Link } from "react-router-dom";
 import ProfileHeader from "./ProfileHeader";
 import ProfileContent from "./ProfileContent";
 import ProfileController from "../../js/ProfileController";
 import { userContext } from "../../contexts/userContext";
 import LocateUser from "../common/LocateUser";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 import "../../style/Profile.css";
 
@@ -13,6 +15,18 @@ const Profile = (props) => {
 
   const url = process.env.REACT_APP_GET_USER_URL;
   const history = useHistory();
+
+  const toggle = () => {
+    const spanMenu = document.querySelector(".profile__menu");
+
+    if (spanMenu.style.display === "none") {
+      spanMenu.style.display = "flex";
+      spanMenu.classList.add("toggled");
+    } else {
+      spanMenu.style.display = "none";
+      spanMenu.classList.remove("toggled");
+    }
+  };
 
   const fetchData = () => {
     fetch(url, {
@@ -65,6 +79,15 @@ const Profile = (props) => {
   if (User.isLoggedIn) {
     return (
       <div className="profile__container">
+        <span class="profile__menu" id="profile__menu">
+          <h4>Profile Settings</h4>
+          <h4>Requests</h4>
+          <h4>Address</h4>
+          <h4>FAQ</h4>
+        </span>
+        <Link onClick={toggle} class="profile__menuButton">
+          <FontAwesomeIcon icon={faBars} focusable />
+        </Link>
         <ProfileHeader />
         <ProfileContent />
         <LocateUser />
